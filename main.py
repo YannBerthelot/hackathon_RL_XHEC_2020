ec08ee6f7c0f76d260e36eef9df8d464af38291a
Yann BERTHERLOT <yann.berthelot@capgemini.com>
2020-11-26 20:53:43 +0100

    update toml and main for vid compatilibty on google collab

--- main.py
+++ main.py
@@ -22,6 +22,27 @@ GROUP_NAME = "ADMIN"
 DATE = str(datetime.datetime.today())
 COMMENT = "First Test"
 
+
+def show_video():
+    mp4list = glob.glob("video/*.mp4")
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, "r+b").read()
+        encoded = base64.b64encode(video)
+        ipythondisplay.display(
+            HTML(
+                data="""<video alt="test" autoplay 
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>""".format(
+                    encoded.decode("ascii")
+                )
+            )
+        )
+    else:
+        print("Could not find video")
+
+
 from gym.wrappers import Monitor
 import glob
 import io
