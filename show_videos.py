b6b1d8053fa13aa505ea458fd189269f03977dea
Yann BERTHERLOT <yann.berthelot@capgemini.com>
2020-11-30 19:13:44 +0100

    added show_videos script

---
+++ show_videos.py
@@ -0,0 +1,35 @@
+from gym.wrappers import Monitor
+import glob
+import io
+import base64
+from IPython.display import HTML
+from pyvirtualdisplay import Display
+from IPython import display as ipythondisplay
+import os
+
+
+def show_video(i):
+    mp4list = glob.glob(f"vids/{i}/*.mp4")
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, "r+b").read()
+        encoded = base64.b64encode(video)
+        ipythondisplay.display(
+            HTML(
+                data="""<video alt="test" autoplay 
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>""".format(
+                    encoded.decode("ascii")
+                )
+            )
+        )
+    else:
+        print("Could not find video")
+
+
+if __name__ == "__main__":
+    number_vids = len(os.listdir("vids"))
+    for i in range(number_vids):
+        print(f"Batch {i+1}")
+        show_video(i + 1)
